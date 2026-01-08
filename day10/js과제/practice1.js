3ad63cd35009c0ba024f05d0be129759c426ff95
Sungeun0318 <kimsungeun0318@naver.com>
2026-01-08 09:39:25 +0900

    피드백 수정 완료

--- day10/js과제/practice1.js
+++ day10/js과제/practice1.js
@@ -15,41 +15,51 @@
 // prompt를 세 번 사용하여 사용자로부터 '아이디', '비밀번호', '이름'을 순서대로 입력받습니다.
 // 입력받은 정보로 하나의 member 객체를 생성하고, members 배열에 저장하여 배열을 콘솔에 출력하시오.
 // 단] 입력받은 아이디가 이미 배열에 존재하면 '존재하는 아이디 입니다'를 출력하고 배열에 등록하지 않습니다.
-const members = [
-    { id: 'user1', password: 'pass1', name: '사용자1' },
-    { id: 'user2', password: 'pass2', name: '사용자2' },
-];
 
-let a = prompt('아이디 : ');
-let b = prompt('비밀번호 : ');
-let c = prompt('이름 : ');
+// const members = [
+//     { id: 'user1', password: 'pass1', name: '사용자1' },
+//     { id: 'user2', password: 'pass2', name: '사용자2' },
+// ];
 
-let member = { id: a, password: b, name: c };
+// let a = prompt('아이디 : ');
+// let b = prompt('비밀번호 : ');
+// let c = prompt('이름 : ');
 
-let d = false;
-for (let i = 0; i < members.length; i++) {
-    if (members[i].id == a) {
-        d = true;
-        break;
-    }
-}
+// let member = { id: a, password: b, name: c };
 
-if (d) {
-    console.log('존재하는 아이디 입니다');
-}
-else {
-    members.push(member);
-}
-console.log(members);
+// let d = false;
+// for (let i = 0; i < members.length; i++) {
+//     if (members[i].id == a) {
+//         d = true;
+//         break;
+//     }
+// }
+
+// if (d) {
+//     console.log('존재하는 아이디 입니다');
+// }
+// else {
+//     members.push(member);
+// }
+// console.log(members);
 
 
 // 문제 3: 객체 배열의 속성 값 평균 구하기
 // scores 배열에 담긴 모든 학생의 수학(math) 점수 평균을 계산하여 콘솔에 출력하시오.
-// const scores = [
-//   { name: 'A', math: 80, science: 92 },
-//   { name: 'B', math: 95, science: 88 },
-//   { name: 'C', math: 76, science: 78 }
-// ];
+
+const scores = [
+  { name: 'A', math: 80, science: 92 },
+  { name: 'B', math: 95, science: 88 },
+  { name: 'C', math: 76, science: 78 }
+];
+
+
+
+
+
+
+
+
 
 // 문제 4: 특정 조건을 만족하는 객체 찾기
 // products 배열에서 id가 3인 상품 객체를 찾아, 해당 객체 전체를 콘솔에 출력하시오. 일치하는 객체가 없으면 "상품을 찾을 수 없습니다."를 출력합니다.
